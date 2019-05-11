8118b678cc14497c0e24894d898f5d988dd1e0d3
UNICE_ETU/TMF <t.michelfoiry@protonmail.com>
2019-05-11 14:15:03 +0200

    Ajout de la barre de menu avec éléments.

--- src/com/itii/planning/gui/MainWindow.java
+++ src/com/itii/planning/gui/MainWindow.java
@@ -1,7 +1,7 @@
 package com.itii.planning.gui;
 
-import javax.swing.Box;
-import javax.swing.JFrame;
+import java.awt.Dimension;
+import javax.swing.*;
 
 public  class   MainWindow
         extends JFrame
@@ -38,14 +38,59 @@ public  class   MainWindow
 
     private void initialize()
     {
-        this.setSize(200, 100);
-        this.setVisible(true);
-
+        Dimension FixedDimension = new Dimension(400,200);
+        this.setSize(400, 200);
+        this.setName("test");
         this.validate();
         this.repaint();
+
+        /*
+        this.setPreferredSize(FixedDimension);
+        this.setMinimumSize(FixedDimension);
+        this.setMaximumSize(FixedDimension);
+        */
+        this.setTitle("Youpi");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        final JMenuBar menuBar = new JMenuBar();
+        this.setJMenuBar(menuBar);
+
+        // Creation des menus dans la barre de menus
+        JMenu fileMenu = new JMenu("File");
+        menuBar.add(fileMenu);
+        JMenu editMenu = new JMenu("Edit");
+        menuBar.add(editMenu);
+        JMenu viewMenu = new JMenu("View");
+        menuBar.add(viewMenu);
+        JMenu helpMenu = new JMenu("Help");
+        menuBar.add(helpMenu);
+
+        // Creation des items dans menu file
+        JMenuItem quitItem = new JMenuItem("Quit");
+        fileMenu.add(quitItem);
+        JMenuItem createItem = new JMenuItem("Create");
+        editMenu.add(createItem);
+        JMenuItem editItem = new JMenuItem("Edit");
+        editMenu.add(editItem);
+        JMenuItem markItem = new JMenuItem("Mark");
+        editMenu.add(markItem);
+        JMenuItem duplicateItem = new JMenuItem("Duplicate");
+        editMenu.add(duplicateItem);
+        JMenuItem deleteItem = new JMenuItem("Delete");
+        editMenu.add(deleteItem);
+
+        // Creation des items dans menu view
+        JMenuItem listItem = new JMenuItem("List");
+        viewMenu.add(listItem);
+        JMenuItem weekItem = new JMenuItem("Week");
+        viewMenu.add(weekItem);
+        JMenuItem monthItem = new JMenuItem("Month");
+        viewMenu.add(monthItem);
+
+        // Creation des panels
+
+        this.setVisible(true);
     }
-
-
 
     private void    _create_layout()
     {
